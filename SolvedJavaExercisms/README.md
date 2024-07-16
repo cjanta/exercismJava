c37cc3893b631b2156ec1914e1717bd833083475
Sven <svkanter@gmail.com>
2024-07-16 16:40:18 +0200

    update

--- SolvedJavaExercisms/README.md
+++ SolvedJavaExercisms/README.md
@@ -2,11 +2,14 @@
 
 # App.java zum testen der Kapitel (nicht vollständig)
 
-# 01 Basics
+# 01 Basics 
 > Lasagne.java
 
-# 02 LogLevels Stringmanipulation
+# 02 LogLevels :Strings I
 > LogLevels.java
 
-# 03 Ausdrücke
+# 03 Ausdrücke :Boolean I
 > AnnalynsInfiltration.java
+
+# 04 If-Else Statements
+> CarsAssemble.java
---
+++ SolvedJavaExercisms/src/CarsAssemble.java
@@ -0,0 +1,18 @@
+public class CarsAssemble {
+
+
+    /**Implement the CarsAssemble.productionRatePerHour() method
+     *  to calculate the assembly line's production rate per hour,
+     *  taking into account its current assembly line's speed 
+     *  quote: Keine Ahnung wird nicht genau beschrieben worum es geht! Ätzend.
+     * @param speed
+     * @return
+     */
+    public double productionRatePerHour(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+}
